6ce3aa9be8d52ae22a727c780cd4748bce76c95e
vatin092548-prog <vatin092548@gmail.com>
2026-08-19 13:30:32 +0700

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "racha-furniture.firebaseapp.com",
+  projectId: "racha-furniture",
+  storageBucket: "racha-furniture.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
