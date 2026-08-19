339292b3575d23516a3be72b9962012579c9f9b7
vatin092548-prog <vatin092548@gmail.com>
2026-08-19 19:23:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -612,7 +612,9 @@ export default function App() {
                 </span>
               </div>
               <div className="flex items-center gap-1 text-xs text-[#D4AF37]">
-                <MapPin className="w-3 h-3" /> สาขาหาดใหญ่ (ถ.สามสิบเมตร)
+                <MapPin className="w-3 h-3" /> สาขาหาดใหญ่ (ถ.สามสิบเมตร) สอบถาม-สั่งซื้อ โทร.074-244665 , 086-4906582
+                                
+
               </div>
             </div>
           </div>
