3dba76a39e09626058c1bc4ad73ec97f582d1b5f
vatin092548-prog <vatin092548@gmail.com>
2026-08-19 13:40:41 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDyRUEJc2MGTV7d4zJAY3EnjgzYQXQHWyg",
   authDomain: "racha-furniture.firebaseapp.com",
   projectId: "racha-furniture",
-  storageBucket: "racha-furniture.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "racha-furniture.firebasestorage.app",
+  messagingSenderId: "686293999389",
+  appId: "1:686293999389:web:9c1cb3d950bc4465cdb77c"
 };
 
 const app = initializeApp(firebaseConfig);
